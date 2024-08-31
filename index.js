3a8f8f6f18e1abae5b92d52923267761dcbab9be
natalie vittini <natalievittini@gmail.com>
2024-08-31 18:45:22 -0400

    added font

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 let saveEl = document.getElementById("save-el");
 
 // let button = 0;
-let button = localStorage.getItem("counter") ? parseInt(localStorage.getItem("counter")) : 0;
-
-
-
-
+let button = localStorage.getItem("counter")
+  ? parseInt(localStorage.getItem("counter"))
+  : 0;
 
 function updateCounter() {
   let counterText = button === 1 ? button + " cup " : button + " cups";
@@ -13,37 +11,36 @@ function updateCounter() {
 
   if (button >= 13) {
     document.getElementById("congrats").innerText = "You hit the daily goal!";
+  } else if (button <= 0) {
+    document.getElementById("congrats").innerText = "Recommended: 13 cups daily!";
   } else {
     document.getElementById("congrats").innerText = "Keep Sipping!";
   }
 }
+
+
 function buttonIncrement() {
   button += 1;
   updateCounter();
   localStorage.setItem("counter", button);
-
 }
 
 function buttonDecrement() {
   button -= 1;
 
-  if(button < 0){
-    button = 0; 
+  if (button < 0) {
+    button = 0;
   }
   updateCounter();
 
   localStorage.setItem("counter", button);
 }
 
-
 function resetCounter() {
-button = 0; 
-updateCounter();
-localStorage.setItem("counter", button);
-
+  button = 0;
+  updateCounter();
+  localStorage.setItem("counter", button);
 }
-
-
 
 document.getElementById("reset").onclick = resetCounter;
 document.getElementById("Decrease").onclick = buttonDecrement;
